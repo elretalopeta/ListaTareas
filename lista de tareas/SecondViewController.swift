35c21631edfdae6dca5ae6fcdf6e273a4da24990
usu27 <usu27@a27mac03b.local>
2016-02-02 16:38:57 +0100

    Initial Commit

---
+++ lista de tareas/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  lista de tareas
+//
+//  Created by usu27 on 2/2/16.
+//  Copyright © 2016 usu27. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
